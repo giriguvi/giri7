617dd3530c396f430e8a7f5d5a8ecc0f8468a6e7
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-09 11:09:57 +0000

    Update post.c

---
+++ post.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i;
+printf("enter the number:");
+scanf("%d",i);
+if(i<0)
+printf("the number is negative");
+elseif(i>0)
+printf("the number is positive");
+else
+printf("zero");
+}
