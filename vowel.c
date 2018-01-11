f08ea23fd036fa53577c9328bfaa513019818684
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-11 10:56:17 +0000

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+scanf("%c",ch);
+print("%c",ch);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+{
+printf("it is vowels");
+}
+else
+{
+printf("it is constant");
+}
+}
