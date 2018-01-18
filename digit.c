3f8b64192b810f3ea77b17d338be03f993a57a0a
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-18 11:15:05 +0000

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i;
+int count=o;
+printf ("enter the numbers:");
+scanf("%d",i);
+while(n!=0)
+{
+n=n/10;
+count=count+n;
+printf("total count is %d",count);
+}
+}
