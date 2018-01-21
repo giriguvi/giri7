b0a264a964c719e1c5c1538e08413ecac7c28ea4
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-21 14:08:26 +0530

    Update begin28.c

--- begin28.c
+++ begin28.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-int main()
+void
 {
-int i,a[10],n;
+int i,a[10],m;
 printf("enter no of elements in an array\n");
-scanf("%d",&n);
+scanf("%d",&m);
 printf("enter the elements in an array:\n");
-for(i=0;i<n;i++)
+for(i=0;i<m;i++)
 {
 scanf("%d\n",&a[i]);
 }
-for(i=0;i<n;i++)
+for(i=0;i<m;i++)
 {
 printf("a[%d]=%d\n",i,a[i]);
 }
-return 0;
 }
