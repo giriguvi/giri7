873637db25831c485e440eab29fc0539111a6c64
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-21 15:16:09 +0530

    Create begin33.java

---
+++ begin33.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class begin33 {
+    
+ public static void main(String[] args) {
+		String test = "hello world";
+		count(test);
+
+	}
+	public static void count(String x){
+		char[] ch = x.toCharArray();
+		int space = 0;
+		for(int i = 0; i < x.length(); i++)
+	 (Character.is SpaceChar(ch[i]))
+{
+				space ++ ;
+		}
+		System.out.println("hello world");
+		System.out.println("space: " + space);
+			}
+}
+
