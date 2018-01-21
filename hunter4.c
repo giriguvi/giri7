773b56341027e07f16c3553c69e25f2853e676e8
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-21 16:25:53 +0530

    Create hunter4.c

---
+++ hunter4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main()
+{
+    int arr[MAX_SIZE], f[MAX_SIZE];
+    int size, i, j, count;
+    scanf("%d", &size);
+   for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+        f[i] = -1;
+    } for(i=0; i<size; i++)
+    {
+        count = 1;
+        for(j=i+1; j<size; j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                count++;
+                f[j] = 0;
+            }
+        }
+        if(f[i]!=0)
+        {
+            f[i] = count;
+        }
+    }
+    for(i=0; i<size; i++)
+    {
+        if(f[i] == 1)
+        {
+            printf("%d ", arr[i]);
+        }
+    }
+}
