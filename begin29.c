9ac5aea0c25cc119e3fbb7daef3c078bf7f82eb9
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-21 14:12:11 +0530

    Create begin29.c

---
+++ begin29.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int min,hour,r;
+printf("enter the hours");
+scanf("%d",&hour);
+printf("enter the minutes");
+scanf("%d",&min);
+r=(hours*60)+minutes;
+printf("hours and minutes are");
+return 0;
+}
