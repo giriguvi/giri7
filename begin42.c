a253dece7a3366090f339b3c84ac11fe2db81ee3
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-25 10:32:27 +0000

    Create begin42.c

---
+++ begin42.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char a[10]="hello";
+char b[10]="gps";
+c=strlen(a);
+d=strlen(b)
+printf("the length is %d%d",&c,&d);
+if(a>b)
+{
+print("the name is %s",a);
+}
+if(a<b)
+{
+printf("the name is %s",b);
+}
+return 0;
+}
