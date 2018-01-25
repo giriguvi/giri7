f731b99dffa143f31afc0adcac052d9cae9e6ad7
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-01-25 10:56:22 +0000

    Create begin44.c

---
+++ begin44.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a<=10)
+	{
+		printf("yes the number is with in the range");
+	}
+	else
+	{
+		printf("the is with in the range");
+	}
+	return 0;
+}
