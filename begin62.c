aa4a3ec506464d3fa1251128c79cc0177fd5ebc0
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-02-13 15:06:17 +0530

    Create begin62.c

---
+++ begin62.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a[50];
+int i;
+printf("enter the string");
+scanf("%s",&a);
+if(a[i]=='1'||a[i]=='0')
+{
+printf("it contains binary value");
+}
+else
+{
+printf("no binary value");
+}
+return 0;
+}
