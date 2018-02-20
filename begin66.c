99593a0b3a36284b7121f2bb045619ba8be1f389
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-02-20 11:22:30 +0000

    Create begin66.c

---
+++ begin66.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("enter the number:");
+    scanf("%d",&i);
+    if(i%2==0)
+    {
+        printf("prime");
+    }
+    else
+    {
+        printf("not prime");
+        
+    }
+    return 0;
+}
