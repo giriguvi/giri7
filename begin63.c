d7c363119feb7ffc26611f75fd77084a8a58db6d
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-02-20 10:44:37 +0000

    Create begin63.c

---
+++ begin63.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,count=0;
+char a100]="hi hello";
+for(i=1;a[i]!='\0';i++)
+{
+if(a[i]== ');
+{
+count++;
+}
+}
+printf("the no of word is",count+1
+return 0;
+}
