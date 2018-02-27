0527999aabc7bcf1637ed21109259b5c57a24c38
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-02-27 16:48:25 +0530

    Create begin75.c

---
+++ begin75.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char y[100],s;
+	int b;
+	printf("enter the string");
+	scanf("%s",y);
+	b=strlen(y);
+	s=b/2;
+	a[s]='*';
+printf("the string is %s",y);
+}
