4707a0276eacb5d6c352de0d14a8dc9b3daf76b0
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-03-02 16:07:55 +0530

    Create begin83.c

---
+++ begin83.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    scanf("%d%d",&a,&b);
+    e=a/b;
+    sacnf("%d%d",&c,&d);
+    f=c%d;
+    printf("the division is---> %d",e);
+    printf("the modulo is---> %d",f);
+    return 0;
+}
