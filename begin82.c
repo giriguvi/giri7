fb6e5be0f150a4937f9d1ec1a8b328a144c13cc9
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-03-02 15:48:37 +0530

    Create begin82.c

---
+++ begin82.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float a,b;
+double c;
+printf("length and breadth of the farnm--->");
+scanf("%f %f",&a,&b);
+c=a*b;
+printf("area of the farm ----> %lf",c);
+return 0;
+}
