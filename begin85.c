e3f7f9e26895e9384d7738a3967ee62764b10df7
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-03-02 16:52:47 +0530

    Create begin85.c

---
+++ begin85.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    
+    char a[20];
+    int i;
+    scanf("%s",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if((i%2)==0)
+        printf("%c",a[i]);
+    }
+    printf("\t");
+    for(i=0;a[i]!='\0',i++)
+    {
+        if((i+1)%2==1)
+        printf("%c",a[i]);
+    }
+}
