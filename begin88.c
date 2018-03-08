b03201e733b9fcd71102a4669ecd920a87ab721e
giriguvi <35258450+giriguvi@users.noreply.github.com>
2018-03-08 16:50:33 +0530

    Create begin88.c

---
+++ begin88.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, i, gcd, lcm;
+
+    printf("Enter two positive integers: ");
+    scanf("%d %d",&a,&b);
+
+    for(i=1; i <= a && i <= b; ++i)
+    {
+
+        if(a%i==0 && b%i==0)
+            gcd = i;
+    }
+
+    lcm = (a*b)/gcd;
+    printf("The LCM of two numbers %d and %d is %d.", n1, n2, lcm);
+
+    return 0;
+}
